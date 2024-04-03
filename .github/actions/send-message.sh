6faf436669a758dba07fefbeebb7c6ca68e24073
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-04-03 23:26:40 +0530

    slack api key passed

--- .github/actions/send-message.sh
+++ .github/actions/send-message.sh
@@ -1,5 +1,5 @@
 echo "Important commit pushed!"
-SLACK_API_URL="https://hooks.slack.com/services/T06S7MSJPK9/B06SN6Z17LK/${{ secrets.SLACK_KEY }}"
+SLACK_API_URL="https://hooks.slack.com/services/T06S7MSJPK9/B06SN6Z17LK/$1"
 MESSAGE="An Important commit pushed :tada:"
 
 curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$MESSAGE\"}" "$SLACK_API_URL"
